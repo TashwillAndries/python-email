8ae77e66bffecca042a1dee2adbf03cc6803212c
TashwillAndries <82204842+TashwillAndries@users.noreply.github.com>
2021-05-28 12:21:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 s = smtplib.SMTP('smtp.gmail.com', 587)
 sender_email_id = 'tashwilla27@gmail.com'
-receiver_email_id = 'thapelo@lifechoices.co.za, jamestjay85@gmail.com, mikaylabeelders@gmail.com, mujaid.kariem22@gmail.com'
+receiver_email_id = ['thapelo@lifechoices.co.za, jamestjay85@gmail.com, mikaylabeelders@gmail.com, mujaid.kariem22@gmail.com', 'nathangiose@gmail.com']
 password = input("Enter Senders Email Password: ")
 
 subject = "Greetings"
 msg = MIMEMultipart()
 msg['from'] = sender_email_id
-msg['To'] = receiver_email_id
+msg['To'] = ', '.join(receiver_email_id)
 msg['Subject'] = subject
 body = "hi guys how are you. i am doing fine\n"
 body = body + "How was your task yesterday"
